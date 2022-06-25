ca95f659c2daa17f9c75ff666d054ee5c6a33b09
assumptaDev <assumptaoseluokoduwa@gmail.com>
2022-06-25 22:21:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    var firstNumber = prompt("Please provide the first number");
+    var secondNumber = prompt("Please provide the second number");
+    var oper = prompt("Please type an operator");
+    if(oper=="+"){
+    alert(Number(firstNumber)+Number(secondNumber));
+    }   
+   else if(oper=="-"){
+        alert(Number(firstNumber)-Number(secondNumber));
+        }   
+       else if(oper=="*"){
+            alert(Number(firstNumber)*Number(secondNumber));
+            }   
+            else if(oper=="/"){
+                alert(Number(firstNumber)/Number(secondNumber));
+                }  
